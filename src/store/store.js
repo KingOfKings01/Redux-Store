4b7128187ed0a5203b75c0f4f7cb661bf1b469d0
KingOfKings01 <87249925+KingOfKings01@users.noreply.github.com>
2023-08-10 22:55:04 +0530

    add productsSlice into store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'   
 import cartSlice from './cartSlice' 
+import productsSlice from './productsSlice'
+
 
 const store = configureStore({
     reducer :{
-        cart:cartSlice
+        cart:cartSlice,
+        products:productsSlice
     }
 })
 
