b99b0eedf637f3c2affdbe5967aef1855810102c
KingOfKings01 <87249925+KingOfKings01@users.noreply.github.com>
2023-08-11 04:03:45 +0530

    add loading and error message on status

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,31 +7,43 @@ import { add } from "../store/cartSlice";
 import { getProducts } from "../store/productsSlice";
 
 export default function Products() {
-  
-  const dispatch = useDispatch()
-  const {data : products} = useSelector(state => state.products)
-  
+  const dispatch = useDispatch();
+  const { data: products, status } = useSelector((state) => state.products);
+
   useEffect(() => {
-    dispatch(getProducts())
+    dispatch(getProducts());
   }, []);
 
+  if (status == "loading") {
+    return <h1>Loading...</h1>;
+  }
+
+  if (status == "error") {
+    return (
+      <div className="text-danger">
+        <h1>Sorry for inconvenience</h1>
+        <h2>We are currently fixing this issue</h2>
+        <h2>Please try after some time</h2>
+      </div>
+    );
+  }
 
   const addToCart = (product) => {
-    dispatch(add(product))
-  }
+    dispatch(add(product));
+  };
 
   const cards = products.map((product, idx) => (
     <div key={idx} className="col-md-3 my-2">
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={product.images[0]} />
         <Card.Body>
-          <Card.Title className="fs-5" >{product.title}</Card.Title>
-          <Card.Text>
-            ${product.price}
-          </Card.Text>
+          <Card.Title className="fs-5">{product.title}</Card.Title>
+          <Card.Text>${product.price}</Card.Text>
         </Card.Body>
         <Card.Footer>
-          <Button variant="primary" onClick={()=> addToCart(product)}>Add to cart</Button>
+          <Button variant="primary" onClick={() => addToCart(product)}>
+            Add to cart
+          </Button>
         </Card.Footer>
       </Card>
     </div>
